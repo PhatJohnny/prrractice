0e893d8d6ce64da60b86c363c758c633c660d708
PhatJohnny <wesley.runner@mtchs.org>
2016-11-07 10:15:14 -0700

    Windows 95
    This is a sicc Windows sim, aesthetic af

---
+++ Win 95/Formail.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Titled Document</title>
+<?php
+$to ="wesley.runner@mtchs.org";
+$subject ="Formail Submission";
+$from =$_GET['emailAddress'};
+$message ="Name: ".$_GET['fullName'}."\nPhone: ".$_GET['phoneNumber'}."\nMessage: ".$_GET['comments'};
+mail($to,$subject,$message,$from);
+
+?>
+<link rel="stylesheet" type="text/css" href="HTMLPRETISTUHF.css">
+
+</head>
+
+<body><main>
+<header class="center"><h1>Bootleg eBay</h1></header>
+<ul><li><a href="http://www.refold.co/pre-order/">DISAPOINTMENT</a></li><li><a href="www.youtube.com/watch?v=Wga5A6R9BJg">FUNKY FRESH</a></li><li><a href="http://www.youredm.com/2015/09/03/dj-uses-unexpected-john-cena-to-troll-the-club/">JOHN CENA</a></li></ul>
+<nav><p>Good job for submitting, we're proud of you!</p>
+</nav>
+<section><strong><h2>Yippie Kay Ey</h2>
+</strong></section>
+
+
+<aside>On a cold winter morning, in the time before the light
+In flames of death's eternal reign we ride towards the fight
+When the darkness has fallen down, and the times are tough all right
+The sound of evil laughter falls around the world tonight
+
+Fighting hard, fighting on for the steel, through the wastelands evermore
+The scattered souls will feel the hell bodies wasted on the shores
+On the blackest plains in hell's domain, we watch them as we go
+In fire and pain, and once again we know
+
+So now we fly ever free
+We're free before the thunderstorm
+On towards the wilderness our quest carries on
+Far beyond the sundown, far beyond the moonlight
+Deep inside our hearts and all our souls
+
+[Chorus:]
+So far away we wait for the day
+For the lives all so wasted and gone
+We feel the pain of a lifetime lost in a thousand days
+Through the fire and the flames we carry on
+
+As the red day is dawning
+And the lightning cracks the sky
+They'll raise their hands to the heavens above
+With resentment in their eyes
+Running back from the mid-morning light
+There's a burning in my heart
+We're banished from a time in a fallen land
+To a life beyond the stars
+
+In your darkest dreams see to believe
+Our destiny is time
+And endlessly we'll all be free tonight
+
+And on the wings of a dream, so far beyond reality
+All alone in desperation, now the time has gone
+Lost inside you'll never find, lost within my own mind
+Day after day this misery must go on
+
+[Chorus]
+
+[Solos: Twin/Sam/Twin/Herman /Twin/Herman/Sam/Herman/Sam]
+
+Now here we stand with their blood on our hands
+We fought so hard now can we understand
+I'll break the seal of this curse if I possibly can
+For freedom of every man
+
+[Chorus]</aside>
+<article><b>Contact Information</b><br> John Felix Anthony Cena (born April 23, 1977) is an American professional wrestler, rapper and actor signed to WWE. Cena is the public face of WWE,[8][9] and has served as its franchise player since 2005.[10]
+
+Cena started his pro wrestling career in 1999 with Ultimate Pro Wrestling, where he held the UPW Heavyweight Championship.[11] Cena signed a developmental contract with the World Wrestling Federation (WWF, later renamed to WWE) in 2001, debuting on the main roster in 2002.</article>
+
+</main>
+</body>
+</html>
